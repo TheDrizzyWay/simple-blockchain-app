26e92d9d439398806c4857aeccebf68f315bbbcb
Christopher Iliyas <christopheriliyas@gmail.com>
2022-03-04 23:09:59 +0100

    complete navigation bar

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,12 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { Loader } from './';
 
 const Welcome = () => {
   return (
-    <div>Welcome</div>
+    <div></div>
   )
 }
 
